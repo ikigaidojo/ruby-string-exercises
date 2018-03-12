e78ac8a17eea4811f717ed173318f91ce010bf24
moniquemanansala29 <moniquemanansala29@gmail.com>
2018-03-12 05:11:38 -0400

    exercise1

---
+++ monique/monique-string-exercise2.rb
@@ -0,0 +1,4 @@
+my_string = "submarine"
+if my_string.include? "cde"
+   puts "String includes 'cde'"
+end
---
+++ monique/monique-string-exercises1.rb
@@ -0,0 +1,18 @@
+
+str = "Monique Manansala"
+
+def capitalize(str)
+    str.capitalize
+end
+
+def downcase(str)
+    str.downcase
+end 
+
+def upcase(str)
+    str.upcase
+end
+    
+puts capitalize(str)
+puts downcase(str)
+puts upcase(str)
