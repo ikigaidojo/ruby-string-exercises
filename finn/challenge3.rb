c0d20e9e0ede939380ac792cc82533e3d8167921
Finn <finn.rothacker@gmail.com>
2018-03-12 16:25:17 +0800

    challenge the completed and saved + empty challenge 4

--- finn/challenge3.rb
+++ finn/challenge3.rb
@@ -1,9 +1,9 @@
 # challenge 3
-wide_string = "J   a     v a    S c     r  i    pt"
+string = "J   a     v a    S c     r  i    pt"
 
-def remove_spaces(wide_string)
-	puts "The entered string was #{wide_string}"
-	puts wide_string.delete(" ")
+def remove_spaces(string)
+	puts "The entered string was #{string}"
+	puts string.delete(" ")
 end
 
 
