b1a5944e948cf950d3af324c97c8e52c2785e898
Finn <finn.rothacker@gmail.com>
2018-03-12 16:23:36 +0800

    challenge 3 completed

---
+++ finn/challenge3.rb
@@ -0,0 +1,11 @@
+# challenge 3
+wide_string = "J   a     v a    S c     r  i    pt"
+
+def remove_spaces(wide_string)
+	puts "The entered string was #{wide_string}"
+	puts wide_string.delete(" ")
+end
+
+
+remove_spaces(wide_string)
+
