0af3347a99095aca9669ccc5f04134ee8b0d857c
moniquemanansala29 <moniquemanansala29@gmail.com>
2018-03-12 22:14:17 -0400

    exercise8

---
+++ monique/monique-string-exercise7.rb
@@ -0,0 +1,5 @@
+def check_string(my_string, chr)
+    return my_string.count(chr) 
+end
+
+print check_string("JavaScript", "J")
---
+++ monique/monique-string-exercise8.rb
@@ -0,0 +1,5 @@
+def check_string(str)
+    return str.chars.sort.join 
+end
+
+print check_string("javascript")
