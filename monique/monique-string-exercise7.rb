3b81e64f56e82f6fc1ba7c0653c7b4590231907b
moniquemanansala29 <moniquemanansala29@gmail.com>
2018-03-12 22:59:48 -0400

    exercise7

--- monique/monique-string-exercise7.rb
+++ monique/monique-string-exercise7.rb
@@ -1,5 +1,16 @@
+def message_prompt(message)
+    print message
+    value = gets.chomp
+    return value
+end
+
 def check_string(my_string, chr)
     return my_string.count(chr) 
 end
 
-print check_string("JavaScript", "J")
+
+string = message_prompt("enter string:") 
+character = message_prompt("what is the answer?")
+
+result = check_string(string, character)
+puts "#{character} #{result}"
